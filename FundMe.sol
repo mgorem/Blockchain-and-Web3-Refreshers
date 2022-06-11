fa935d4facb263bee4c2d5b0fc449efde467c522
mgorem <oremgitonga22@gmail.com>
2022-06-11 20:28:45 +0300

    require, msg.value and revert

--- FundMe.sol
+++ FundMe.sol
@@ -1,9 +1,22 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
 // Contract that allows people to fund collective goods
 // People can send Ethereum, Polygon, Avalanche to this contract
 // The owner can withdraw the funds and do whatever they want
-contract FundMe {
 
+// Gets funds from users
+// Withdraw funds
+//Set a minimum funding value in usd
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FundMe {
+    function fund() public payable {
+        // Set a minimum fund amount in USD
+        // Contract addresses can hold funds
+        require(msg.value > 1e18, "Sorry, didn't send enough."); // 1e18 == 1* 10 ** 18 == 1000000000000000000
+    }
+
+    // function withdraw() public {
+
+    // }
 }
