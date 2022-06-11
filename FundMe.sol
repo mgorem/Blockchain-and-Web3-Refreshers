f79fed555bc36ed9307c47ee26f3ce427400ab36
mgorem <oremgitonga22@gmail.com>
2022-06-11 19:40:05 +0300

    create and licence initial files

---
+++ FundMe.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+// Contract that allows people to fund collective goods
+// People can send Ethereum, Polygon, Avalanche to this contract
+// The owner can withdraw the funds and do whatever they want
+contract FundMe {
+
+}
