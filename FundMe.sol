fb52d27a59e238b7a0b5a7cb020ef8641d1630da
mgorem <oremgitonga22@gmail.com>
2022-06-11 21:04:57 +0300

    use chainlink data feeds get ethereum price

--- FundMe.sol
+++ FundMe.sol
@@ -1,3 +1,5 @@
+// Blockchain Oracles: Any device that interacts with the offchain
+// world to provide data or external computation to the smart contract
 // Contract that allows people to fund collective goods
 // People can send Ethereum, Polygon, Avalanche to this contract
 // The owner can withdraw the funds and do whatever they want
@@ -10,10 +12,13 @@
 pragma solidity ^0.8.0;
 
 contract FundMe {
+    
+    uint256 public minimumUsd = 50;
+
     function fund() public payable {
         // Set a minimum fund amount in USD
         // Contract addresses can hold funds
-        require(msg.value > 1e18, "Sorry, didn't send enough."); // 1e18 == 1* 10 ** 18 == 1000000000000000000
+        require(msg.value >= minimumUsd, "Sorry, didn't send enough."); // 1e18 == 1* 10 ** 18 == 1000000000000000000
     }
 
     // function withdraw() public {
