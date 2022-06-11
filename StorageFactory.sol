b05e8f5fde8b9bc79ec3c8176a9d49347ffe3741
mgorem <oremgitonga22@gmail.com>
2022-06-11 18:57:24 +0300

    create a contrac to manage other contracts

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,13 +5,24 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage[] public simpleStorageArray;
+    SimpleStorage[] public simpleStorageArray; // Global variable of type SimpleStorageContract, visibility and variable name
 
-    // a function that can deploy the SimpleStorage contract
+    // a function that creates and deploys the SimpleStorage contract
     // Also keeps a running list of our deployed SimpleStorage contracts
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
-        simpleStorage.push(simpleStorage);
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    // Calling other functions from other contracts
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Needs Address of the Contract and ABI of contract
+        // ABI tells our code how to interact with the contract- All inputs and outputs and everything you can do with the contract 
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 
 }
