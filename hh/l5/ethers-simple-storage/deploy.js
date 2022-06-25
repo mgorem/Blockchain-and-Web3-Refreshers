daca037f4bdcf0585b473a9f158526ee9d88ef78
mgorem <oremgitonga22@gmail.com>
2022-06-25 23:03:17 +0300

    deploy contract locally use ganache

--- hh/l5/ethers-simple-storage/deploy.js
+++ hh/l5/ethers-simple-storage/deploy.js
@@ -1,7 +1,26 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function main() {
   // compile contracts in our code
   // compile our contracts seperately using solcjs command
   // solcjs --bin --abi --include-path node_modules/ --base-path . -o . SimpleStorage.sol
+  // http://0.0.0.0:8545
+  // How the script connects too the local blockchain
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet("Enter private-key", provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  // Object used in ethers to deploy contracts
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy(); // wait for contract to deploy hence resolving the promise contract
+  console.log(contract);
 }
 
 main()
