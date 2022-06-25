4b13cc5c8614c74dba0f265323e4717495b146f0
mgorem <oremgitonga22@gmail.com>
2022-06-25 05:25:08 +0300

    price-conversion, transact contract

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 
 import "./SimpleStorage.sol";
 
+// Use virtual in the main function in the first contract
+// Use override in the second function that overrides the initial one
 // Inherits all the functions of the SimpleStorage contract
 contract ExtraStorage is SimpleStorage {
     // overriding functions using  virtual and override
--- FundMe.sol
+++ FundMe.sol
@@ -1,27 +1,86 @@
 // Blockchain Oracles: Any device that interacts with the offchain
-// world to provide data or external computation to the smart contract
+// world to provide data or external computation to the smart contract.
 // Contract that allows people to fund collective goods
 // People can send Ethereum, Polygon, Avalanche to this contract
 // The owner can withdraw the funds and do whatever they want
 
-// Gets funds from users
-// Withdraw funds
-//Set a minimum funding value in usd
+// Gets funds from users into our smart contract
+// Withdraw funds from the smart contract
+// Set a minimum funding value in usd
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract FundMe {
-    
-    uint256 public minimumUsd = 50;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
-    function fund() public payable {
-        // Set a minimum fund amount in USD
-        // Contract addresses can hold funds
-        require(msg.value >= minimumUsd, "Sorry, didn't send enough."); // 1e18 == 1* 10 ** 18 == 1000000000000000000
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    
+    constructor() {
+        i_owner = msg.sender;
     }
 
-    // function withdraw() public {
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+    
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+    
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+    
+    function withdraw() payable onlyOwner public {
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \ 
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback() 
+    //     /   \ 
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
 
-    // }
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+
 }
